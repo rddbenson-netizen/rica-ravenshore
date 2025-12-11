d66b54a5727dcecd9a3d19b495c9f70bc8820f38
rddbenson-netizen <rddbenson@gmail.com>
2025-12-11 14:14:24 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
